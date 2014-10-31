886f6f0c81c868d77273c1624c4cafa47b56fac3
kamil-r <kamil.ras1@gmail.com>
2014-10-31 11:00:02 +0100

    Problem 32 solved.

---
+++ src/main/scala/arithmetic/Problem32.scala
@@ -0,0 +1,22 @@
+package arithmetic
+
+/**
+ * Created by T530 on 2014-10-31.
+ */
+object Problem32 {
+  def gcdDivide(x: Int, y: Int): Int = {
+    val (greater, lower) = if (x >= y) (x, y) else (y, x)
+    val rest = greater % lower
+    if (rest == 0) lower else gcdDivide(lower, rest)
+  }
+  
+  def gcdDivide2(x: Int, y: Int): Int = {
+    if (y == 0) x else gcdDivide2(y, x % y)
+  }
+
+  def gcdSubstract(x: Int, y: Int): Int = {
+    val (greater, lower) = if (x >= y) (x, y) else (y, x)
+    if (lower == 0) lower else gcdDivide(lower, greater - lower)
+  }
+
+}
